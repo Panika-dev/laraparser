c8da59e5127dad0408e9f57f3819ef951cfc7bdb
vagrant <vagrant@homestead>
2018-01-25 10:52:03 +0000

    Init laraparser

---
+++ app/Models/Page.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Page extends Model {
+
+	protected $table = 'pages';
+
+	protected $fillable = [
+		'oid',
+		'url',
+		'html',
+		'data',
+		'status',
+	];
+}
---
+++ database/migrations/2018_01_25_101713_create_pages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+
+	        $table->integer('oid')->nullable();
+	        $table->string('url')->nullable();
+	        $table->text('html')->nullable();
+	        $table->text('data')->nullable();
+	        $table->integer('status')->nullable();
+
+	        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
